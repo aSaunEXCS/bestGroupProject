7bdba994b3088a262f0889fb7829dae09026bb85
MystaMacroon <db751@exeter.ac.uk>
2025-03-20 00:09:51 +0000

    Added UserProfile App
    Unfinished but works

---
+++ groupProjectParent/UserProfile/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import ProfileForm
+
+@login_required
+def profile_view(request):
+    profile = request.user.profile
+    if request.method == "POST":
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=profile)
+    return render(request, 'UserProfile/UserProfile.html', {'form': form, 'profile': profile})
