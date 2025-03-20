0ff922bb451eb8feff39254637c5629693ad379a
MystaMacroon <db751@exeter.ac.uk>
2025-03-20 00:43:20 +0000

    Update UserProfile.views.py
    Made it so legacy users will be updated to have a profile... Hopefully

--- groupProjectParent/UserProfile/views.py
+++ groupProjectParent/UserProfile/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import ProfileForm
+from .models import Profile
 
 @login_required
 def profile_view(request):
-    profile = request.user.profile
+    profile, created = Profile.objects.get_or_create(user=request.user)
     if request.method == "POST":
         form = ProfileForm(request.POST, request.FILES, instance=profile)
         if form.is_valid():
