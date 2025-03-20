65c036ef7fe743167301403794920653f404e342
MystaMacroon <db751@exeter.ac.uk>
2025-03-20 00:13:04 +0000

    Update urls.py
    Added profile to urls

--- groupProjectParent/groupProject/urls.py
+++ groupProjectParent/groupProject/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('', include('homepage.urls')),
     path('', include('leaderboard.urls')),
     path('', include('quizes.urls')),
+    path('profile/', include('UserProfile.urls')),
     path('EnergyConservationMinigame/', include('EnergyConservationMinigame.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
